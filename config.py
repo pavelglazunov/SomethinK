40093de36432fdbd970db514548ad9240e8517b8
Pavel_Glazunov <p6282813@yandex.ru>
2023-07-31 18:53:36 +0300

    fix

--- config.py
+++ config.py
@@ -8,15 +8,14 @@ class BaseConfig:
     JWT_SECRET_KEY = 'super-secret'
     RECAPTCHA_PUBLIC_KEY = "6LftGX8lAAAAACiqSptlqEiqmsU0lhAwOLSKOFnR"
     RECAPTCHA_PRIVATE_KEY = "6LftGX8lAAAAAP6c0fTEa5K0b0gOQCOCfEwb2NrT"
-    CODE_SALT = "salt"
 
     # настройка Flask-Mail
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
+    MAIL_PORT = 465
     MAIL_USE_TLS = True
     MAIL_DEBUG = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'confirm@somethinkbots.ru'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'qAMWkm9LcWuATCLsAWiT'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'somethinkbots@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'vdlhtksjgfpqavno'
     MAIL_DEFAULT_SENDER = MAIL_USERNAME
 
     # настройка SQL
